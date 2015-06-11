4e1cfd20ce15283a286e35fe17dd3739484e71fb
Jessica Grebenschikov <jessica.greben1@gmail.com>
2015-06-11 12:19:00 -0700

    modify seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,15 @@ Box.create(user_id: 4)
 Box.create(user_id: 5)
 
 
-User.create(first_name: 'Jessica', last_name: 'Grebby', email: 'jessica@jessica.com', encrypted_password:  'jessica')
+User.create(first_name: 'Jessica', last_name: 'Grebby', email: 'jessica@jessica.com', password:  'jessica123')
 
-User.create(first_name: 'Lukas', last_name: 'Lulu', email: 'lukas@luaks.com', encrypted_password:  'lukas')
+User.create(first_name: 'Lukas', last_name: 'Lulu', email: 'lukas@luaks.com', password:  'lukas1234')
 
-User.create(first_name: 'Ricky', last_name: 'Chen', email: 'ricky@ricky.com', encrypted_password:  'ricky')
+User.create(first_name: 'Ricky', last_name: 'Chen', email: 'ricky@ricky.com', password:  'ricky1234')
 
-User.create(first_name: 'Bob', last_name: 'Bobby', email: 'bob@bob.com', encrypted_password:  'bob')
+User.create(first_name: 'Bob', last_name: 'Bobby', email: 'bob@bob.com', password:  'bob123456')
 
-User.create(first_name: 'Jake', last_name: 'Jakey', email: 'jake@jake.com', encrypted_password:  'jake')
+User.create(first_name: 'Jake', last_name: 'Jakey', email: 'jake@jake.com', password:  'jake12345')
 
 
 annies_mac = Item.create(name: 'Annie\'s Mac \& Cheese', img_name: "/assets/annies_mac.jpg", price: 3.49)
