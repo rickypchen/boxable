b1b0fb799fdf2aee7a19f9ab64e543d94f115d2b
rickypchen <richard.polin.chen@gmail.com>
2015-06-11 15:52:25 -0700

    finished styling

--- app/assets/javascripts/components/item.js.jsx
+++ app/assets/javascripts/components/item.js.jsx
@@ -1,15 +1,18 @@
 var Item = React.createClass({
   render: function(){
     return (
-      <div className = "item">
-        <div className = "itemImage">
+      <div className = "item col-sm-6 col-md-4">
+        <div className = "itemImage thumbnail">
           <img src={this.props.image} />
         </div>
-        <span>{this.props.name}</span>
-        <span>${this.props.price}</span>
+        <div class="caption">
+          <h3>{this.props.name}</h3>
+          <span>${this.props.price}</span>
+          <p>
+            <a href="#" className="btn btn-primary" role="button">Add To Cart</a>
+            <a href="#" className="btn btn-default" role="button">Remove From Cart</a></p>
+        </div>
       </div>
       )
   }
 })
-
-
