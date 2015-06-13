2ed184c5b43533fdcab6a796269d8cd5651c82cf
rickypchen <richard.polin.chen@gmail.com>
2015-06-13 10:47:19 -0700

    added cart total display

--- app/assets/javascripts/components/cart.js.jsx
+++ app/assets/javascripts/components/cart.js.jsx
@@ -1,19 +1,21 @@
 var Cart = React.createClass({
-	render: function(){
+  render: function(){
     var cartArray = [];
-    var cart = this.props.cart
+    var cart = this.props.cart;
+    var totalCheckoutPrice = 0;
     for (var key in cart) {
       if (cart[key]["quantity"] > 0)
         cartArray.push(<CartItem price={cart[key]["itemInfo"]["price"]} name={cart[key]["itemInfo"]["name"]} quantity={cart[key]["quantity"]} />)
-
+        totalCheckoutPrice += cart[key]["itemInfo"]["price"] * cart[key]["quantity"]
     }
+
     console.log(cartArray);
     console.log(this.props.cart);
 
     return (
 	      <div className="cart" >
                   <div className="inventory">
-                    <h2>Cart Summary</h2>
+                    <h2>Box Summary</h2>
                     <table className="summary">
                       <tr>
                         <th>Item</th>
@@ -24,6 +26,7 @@ var Cart = React.createClass({
                     </table>
                   </div>
 	      	<div className='checkout-container'>
+                  <h3>Total: ${totalCheckoutPrice.toFixed(2)}</h3>
 	      	<button type="button" className="btn btn-warning" id="checkout-btn">Checkout</button>
       	        </div>
 	      </div>
