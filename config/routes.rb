ba5f23fe07ea07742e18f82f8b3b20c5a435a9fd
rickypchen <richard.polin.chen@gmail.com>
2015-06-23 14:18:31 -0700

    created delivery form

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'store/load_items' => 'store#load_items'
   get '/checkout' => 'store#checkout'
   post '/box' => 'boxes#create'
+  post '/deliver' => 'boxes#deliver'
 
   devise_for :users, controllers: {
     sessions: 'user/sessions',
